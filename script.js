825423924393a8a4089102b568a475cb6dbbfe72
dav1ch1n <dgliceras@gmail.com>
2026-06-02 11:42:27 +0200

    Contacto ordenado

--- script.js
+++ script.js
@@ -10,24 +10,5 @@ function mostrarTexto(idBoton, idTexto) {
         button2.textContent = "Ver más";
     }
 }
-// for (let i = 0; i < button.length; i++) {
-//     button[i].addEventListener("click", function () {
-//         if (texto[i].style.display === "none") {
-//             texto[i].style.display = "block";
-//             button[i].textContent = " Ver menos";
-//         } else {
-//             texto[i].style.display = "none";
-//             button[i].textContent = "Ver más";
-//         }
-//     });
-// }
-// button[0].addEventListener("click", function () {
-//     if (texto.style.display === "none") {
-//         texto.style.display = "block";
-//         button[0].textContent = " Ver menos";
-//     } else {
-//         texto.style.display = "none";
-//         button[0].textContent = "Ver más";
-//     }
-// });
+
 
